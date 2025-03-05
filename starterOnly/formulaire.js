e37107a30f86a5c6bfe288c2c18a6054a723afa6
aalexandree-g <aalexandree.g@gmail.com>
2025-03-06 00:07:56 +0100

    Ajout validation ou messages d'erreur, fixes #3

--- starterOnly/formulaire.js
+++ starterOnly/formulaire.js
@@ -3,37 +3,45 @@ formulaire.addEventListener("submit", (event) => {
     // on empêche le rechargement du formulaire
     event.preventDefault()
     // on stocke les données renseignées dans formUtilisateur
-    // id et msgErreur sont là afin de gérer les erreurs
     let formUtilisateur = [
         {
-            id : "first",
+            id : "first",       // champ du prénom
             valeur : document.getElementById("first").value,
+            regle : /^[a-zA-Z]{2,}(-[a-zA-Z]+)?$/,
             msgErreur : "Veuillez entrer 2 caractères ou plus pour le prénom."
         },
         {
-            id : "last",
+            id : "last",        // champ du nom
             valeur : document.getElementById("last").value,
+            regle : /^[a-zA-Z]{2,}(-[a-zA-Z]+)?$/,
             msgErreur : "Veuillez entrer 2 caractères ou plus pour le nom."
         },
         {
-            id : "email",
+            id : "email",       // champ de l'email
             valeur : document.getElementById("email").value,
+            regle : /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+$/,
             msgErreur : "L'adresse email est incorrecte."
         },
         {
-            id : "birthdate",
+            id : "birthdate",   // champ de la date de naissance
             valeur : document.getElementById("birthdate").value,
             msgErreur : "Veuillez entrer votre date de naissance."
         },
         {
-            id : "quantity",
+            id : "quantity",    // champ du nombre de concours
             valeur : document.getElementById("quantity").value,
+            regle : /^[0-9]+$/,
             msgErreur : "Veuillez entrer un nombre."
         },
         {
-            id : "location",
-            valeur : document.getElementsByName("location"),
+            id : "location1",   // choix de la ville de participation
+            valeur : document.querySelector('input[name="location"]:checked'),
             msgErreur : "Veuillez choisir une option."
+        },
+        {
+            id : "checkbox1",   // checkbox des conditions générales
+            valeur : document.getElementById("checkbox1"),
+            msgErreur : "Veuillez accepter les termes et conditions."
         }
     ]
     verifierFormulaire(formUtilisateur)
@@ -44,36 +52,23 @@ function verifierFormulaire(formUtilisateur) {
     // s'il y a déjà des messages d'erreur sous les champs, on les supprime
     document.querySelectorAll(".erreur").forEach((msg) => msg.remove())
 
-    // VÉRIFICATION DES CHAMPS AVEC RÈGLES
-    const regles = {
-        first : /^[a-zA-Z]{2,}(-[a-zA-Z]+)?$/, // règle pour le prénom
-        last : /^[a-zA-Z]{2,}(-[a-zA-Z]+)?$/, // règle pour le nom
-        email : /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+$/, // règle pour l'email
-        quantity : /^[0-9]+$/ // règle pour le nombre de concours
-    }
-    // on va balayer l'id de chaque champ de formUtilisateur
+    // on va balayer chaque objet de formUtilisateur
     formUtilisateur.forEach((champ) => {
-        // on les compare aux clés des règles
-        if ((champ.id in regles) && (!regles[champ.id].test(champ.valeur))) {
+        if (
+            // pour le cas des champs avec règles
+            champ.regle && !champ.regle.test(champ.valeur) ||
+            // pour le cas de la date de naissance
+            (champ.id === "birthdate" && champ.valeur === "") ||
+            // pour le cas des boutons radio
+            (champ.id === "location1" && champ.valeur === null) ||
+            // pour le cas des conditions générales
+            (champ.id === "checkbox1" && !champ.valeur.checked)
+        ) {
             afficherMessageErreur(champ.id, champ.msgErreur)
         }
     })
-
-    // VÉRIFICATION DE LA DATE DE NAISSANCE
-    let champNaissance = document.getElementById("birthdate").value
-    if (champNaissance === "") {
-        afficherMessageErreur("birthdate", "Veuillez renseigner votre date de naissance.")
-    }
-
-    // VÉRIFICATION DES BOUTONS RADIO
-
-
-    // VÉRIFICATION DES CONDITIONS GÉNÉRALES
-    let caseConditionsGenerales = document.getElementById("checkbox1")
-    if (!caseConditionsGenerales.checked) {
-        afficherMessageErreur("checkbox1", "Veuillez accepter les termes et conditions.")
-    }
 }
+
 
 // afficher le message d'erreur sous le champ concerné
 function afficherMessageErreur(champId, message) {
@@ -86,5 +81,7 @@ function afficherMessageErreur(champId, message) {
         // on insère la balise à la fin de chaque div de classe formData
         let elementParent = champ.closest(".formData")
         elementParent.appendChild(messageErreur)
+    } else {
+        console.log(`Erreur : aucun balise avec l'id ${champId} n'a été trouvée.`)
     }
 }
